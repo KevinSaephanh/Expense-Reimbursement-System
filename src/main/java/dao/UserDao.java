397fa583aa7d7e424ef1dd4d2ad1e7349d11839b
Kevin <Kevin_Saephanh@yahoo.com>
2019-10-29 15:38:10 -0400

    Second commit

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,5 +1,19 @@
 package dao;
 
-public class UserDao {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import models.User;
+import utils.ConnectionUtil;
+
+public class UserDao {
+	public User login(User user) {
+		try (Connection conn = ConnectionUtil.getConnection()) {
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
